9c0bfa7a9167469eb480dc6547197c3188bd2a5b
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-03-08 19:53:03 -0500

    Update project0.s
    Wrote code for exiting the program. Defined a function Print_Str for printing a string.

--- project0.s
+++ project0.s
@@ -7,3 +7,14 @@ main:
 	
 	# id % N = 03043178 % 7 = 5 
 	# My starting index value is (5+m)%9 where m = 0, 1, 2, 3, 4 ,...., 8 for 9 strings respectively.
+
+
+
+# Exit the program																																																						
+	li $v0, 10			# system call to stop the program		
+	syscall				# close the program
+
+Print_Str:
+	li $v0, 4			# system call for printing a string
+	syscall				# print the string
+	jr $ra				# jump register
