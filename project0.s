9cd988261a02dc0eb2d83b69a11ddfac10ca5fc5
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-03-08 19:45:01 -0500

    Update project0.s
    Loaded the address of the ID in $t0 register. Found out N and defined the starting index for each strings.

--- project0.s
+++ project0.s
@@ -1,2 +1,9 @@
 .data
 id: .asciiz "@03043178"
+
+.text
+main:
+	la $t0, id			# Load the address of the string in id
+	
+	# id % N = 03043178 % 7 = 5 
+	# My starting index value is (5+m)%9 where m = 0, 1, 2, 3, 4 ,...., 8 for 9 strings respectively.
